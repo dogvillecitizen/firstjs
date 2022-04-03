2f774ecface7c40000f84747edde4aa140ee1ca7
Ia Sichinava <iasichinavanew@gmail.com>
2022-04-03 12:50:43 -0700

    files and main tasks

---
+++ app.js
@@ -0,0 +1,27 @@
+let userName = "username";
+let userAge = 30;
+let middleName;
+let studiesJs = true;
+let myNull = null;
+let myNaN = NaN;
+
+let myName = "ia" 
+let myHobby = "learning languages"
+let yearOfBirth  = 1991;
+let currentYear = 2022;
+let myage = currentYear - yearOfBirth;
+
+
+console.log(userName);
+console.log(userAge);
+console.log(middleName)
+console.log(studiesJs);
+console.log(myNull);
+console.log(myNaN);
+console.log(myage);
+
+
+console.log("my nme is" + ' ' +  myName + ' ' + "I am" + ' ' + myage + ' ' + "years old and my hobby is" + ' '  + myHobby);
+let secondWay = `my name is ${myName} i am  ${myage}  years old and my hobby is ${myHobby}`;
+
+console.log(secondWay)
